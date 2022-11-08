cba1bcd7aae9aa3f13625b7d4e9cd53f09f13456
Faridhosen4563 <faridhosen790@gmail.com>
2022-11-08 19:20:14 +0600

    add id dynamic route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,16 +22,29 @@ async function run() {
   try {
     app.get("/services", async (req, res) => {
       const limit = parseInt(req.query.limit);
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+
       const query = {};
       const cursor = serviceCollection.find(query);
       if (limit) {
         const services = await cursor.limit(limit).toArray();
         res.send(services);
       } else {
-        const services = await cursor.toArray();
-        const count = serviceCollection.estimatedDocumentCount();
+        const services = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+        const count = await serviceCollection.estimatedDocumentCount();
         res.send({ count, services });
       }
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
     });
   } finally {
   }
